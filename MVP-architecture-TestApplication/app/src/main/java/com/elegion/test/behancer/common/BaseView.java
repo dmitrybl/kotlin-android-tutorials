ef82a655e137a1399601dc50388cb5130aeeb728
Dmitry <dmitry.bl.5967@gmail.com>
2019-10-12 15:45:12 +0300

    12.10.2019

---
+++ MVP-architecture-TestApplication/app/src/main/java/com/elegion/test/behancer/common/BaseView.java
@@ -0,0 +1,11 @@
+package com.elegion.test.behancer.common;
+
+public interface BaseView {
+
+    void showLoading();
+
+    void hideLoading();
+
+    void showError();
+
+}
---
+++ MVP-architecture-TestApplication/app/src/main/java/com/elegion/test/behancer/ui/projects/ProjectsPresenter.java
@@ -0,0 +1,45 @@
+package com.elegion.test.behancer.ui.projects;
+
+import android.view.View;
+
+import com.elegion.test.behancer.BuildConfig;
+import com.elegion.test.behancer.common.BasePresenter;
+import com.elegion.test.behancer.data.Storage;
+import com.elegion.test.behancer.utils.ApiUtils;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class ProjectsPresenter extends BasePresenter {
+
+    private ProjectsView mView;
+    private Storage mStorage;
+
+    public ProjectsPresenter(ProjectsView mView, Storage mStorage) {
+        this.mView = mView;
+        this.mStorage = mStorage;
+    }
+
+    public void getProjects() {
+        mCompositeDisposable.add(ApiUtils.getApiService().getProjects(BuildConfig.API_QUERY)
+                .doOnSuccess(response -> mStorage.insertProjects(response))
+                .onErrorReturn(throwable ->
+                        ApiUtils.NETWORK_EXCEPTIONS.contains(throwable.getClass()) ? mStorage.getProjects() : null)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(disposable -> mView.showLoading())
+                .doFinally(() -> mView.hideLoading())
+                .subscribe(
+                        response -> {
+                            mView.showProjects(response.getProjects());
+                        },
+                        throwable -> {
+                            mView.showError();
+                        }));
+    }
+
+    public void openProfileFragment(String username) {
+        mView.openProfileFragment(username);
+    }
+
+}
