c169cc895bc0b947216b1801d2b7255d8f179085
dmitrybelyaev <dmitry.bl.5967@gmail.com>
2019-11-25 21:40:47 +0300

    25.11.2019

--- MovieMVVM/app/src/main/java/com/oxcoding/moviemvvm/data/api/TheMovieDBClient.kt
+++ MovieMVVM/app/src/main/java/com/oxcoding/moviemvvm/data/api/TheMovieDBClient.kt
@@ -7,7 +7,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-const val API_KEY = "6e63c2317fbe963d76c3bdc2b785f6d1"
+const val API_KEY = "82631444688ab7fb133fee5667b04ea3"
 const val BASE_URL = "https://api.themoviedb.org/3/"
 
 const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
